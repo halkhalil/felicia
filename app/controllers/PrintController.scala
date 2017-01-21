d53e6f76444f41a20b963e4d08574416d24b741d
Marcin Lawrowski <marcin.lawrowski@gmail.com>
2017-01-21 22:04:39 +0100

    Invoice view refactoring

--- app/controllers/PrintController.scala
+++ app/controllers/PrintController.scala
@@ -21,6 +21,7 @@ import play.api.i18n.Lang
 import play.Logger
 import models.invoice.InvoicePart
 import services.templates.ExternalTemplatesService
+import controllers.viewinput.InvoiceViewInput
 
 @Singleton
 class PrintController @Inject() (
@@ -97,80 +98,11 @@ class PrintController @Inject() (
 	*/
 	def invoice(id: Int, targetCurrency: String, language: String) = (UserAction andThen AuthorizationCheckAction) { request =>
 		implicit val lang = Lang(language) // this implicit value is passed to the template and used by Messages objects
-		val targetCurrencyUpperCase = targetCurrency.toUpperCase()
-		val invoiceOption: Option[Invoice] = invoicesService.get(id)
-		val locale = new Locale(language, language)
-		val pricesFormatter = NumberFormat.getIntegerInstance(locale)
-		pricesFormatter.setMinimumFractionDigits(2)
-		pricesFormatter.setMaximumFractionDigits(2)
-		val quantityFormatter = NumberFormat.getIntegerInstance(locale)
-		quantityFormatter.setMaximumFractionDigits(2)
-		val curencyRateFormatter = NumberFormat.getIntegerInstance(locale)
-		curencyRateFormatter.setMinimumFractionDigits(4)
-		curencyRateFormatter.setMaximumFractionDigits(4)
-		
-		
-		def quantityAsString(invoicePart: InvoicePart): String = {
-			quantityFormatter.format(BigDecimal(invoicePart.quantity) / InvoicePart.Multipler)
-		}
-		
-		def unitPriceAsString(invoicePart: InvoicePart): String = {
-			pricesFormatter.format(BigDecimal(invoicePart.unitPrice) / InvoicePart.Multipler).concat(" ").concat(invoiceOption.get.currency)
-		}
-		
-		def totalAsString(invoicePart: InvoicePart): String = {
-			pricesFormatter.format(BigDecimal(invoicePart.total) / InvoicePart.Multipler).concat(" ").concat(invoiceOption.get.currency)
-		}
-		
-		def invoiceTotalAsString(invoice: Invoice): String = {
-			pricesFormatter.format(BigDecimal(invoice.total) / Invoice.Multipler).concat(" ").concat(invoice.currency)
-		}
-		
-		def currencyRate(invoice: Invoice): Option[BigDecimal] = {
-			if (targetCurrencyUpperCase == invoice.currency.toUpperCase())
-				Some(BigDecimal(1))
-			else
-				currenciesService.getFromPreviousDay(invoice.issueDate, targetCurrency, invoice.currency).map { currencyRate => 
-					Option(BigDecimal(currencyRate.rate) / CurrencyRate.MultiplerValue)
-				}.getOrElse(None)
-		}
-		
-		def currencyRateAsString(invoice: Invoice): Option[String] = {
-			currencyRate(invoice).map { rateValue =>
-				Some(curencyRateFormatter.format(rateValue))
-			}.getOrElse(None)
-		}
-		
-		def currencyRateDateAsDate(invoice: Invoice): Option[Date] = {
-			currenciesService.getFromPreviousDay(invoice.issueDate, targetCurrency, invoice.currency).map { currencyRate => 
-				Option(currencyRate.day)
-			}.getOrElse(None)
-		}
-		
-		def invoiceTotalConverted(invoice: Invoice): Option[BigDecimal] = {
-			currencyRate(invoice).map { rateValue =>
-				Some(BigDecimal(invoice.total) / Invoice.Multipler * rateValue)
-			}.getOrElse(None)
-		}
-		
-		def invoiceTotalConvertedAsString(invoice: Invoice): Option[String] = {
-			invoiceTotalConverted(invoice).map { amount =>
-				Some(pricesFormatter.format(amount).concat(" ").concat(targetCurrencyUpperCase))
-			}.getOrElse(None)
-		}
-		
-		invoiceOption.map { invoice =>
-			val paymentSupplementary: Option[String] = externalTemplatesService.get(
-				"payments.".concat(invoice.paymentMethod.symbol).concat("-").concat(language.toLowerCase())
-			).map { template =>
-				Some(template)
-			}.getOrElse(None)
-			
+
+		invoicesService.get(id).map { invoice =>
 			Ok(
 				views.html.print.invoices.invoice(
-					invoice, targetCurrencyUpperCase, quantityAsString, unitPriceAsString,
-					totalAsString, invoiceTotalAsString(invoice), invoiceTotalConvertedAsString(invoice), currencyRateAsString(invoice),
-					currencyRateDateAsDate(invoice), paymentSupplementary
+					new InvoiceViewInput(externalTemplatesService, currenciesService, invoice, targetCurrency, language)
 				)
 			)
 		}.getOrElse(notFound("Invoice does not exist"))
---
+++ app/controllers/viewinput/InvoiceViewInput.scala
@@ -0,0 +1,96 @@
+package controllers.viewinput
+
+import models.invoice.Invoice
+import java.util.Locale
+import java.text.NumberFormat
+import play.api._
+import play.api.mvc._
+import services.ApplicationService
+import play.api.libs.json.Json
+import services.users.AuthenticationService
+import services.users.UserService
+import services.InvoicesService
+import services.currencies.CurrenciesService
+import java.util.Date
+import models.invoice.Invoice
+import models.CurrencyRate
+import play.api.i18n.I18nSupport
+import play.api.i18n.Messages.Implicits._
+import play.api.i18n.MessagesApi
+import play.api.i18n.Lang
+import play.Logger
+import models.invoice.InvoicePart
+import services.templates.ExternalTemplatesService
+
+class InvoiceViewInput(
+		externalTemplatesService: ExternalTemplatesService, currenciesService: CurrenciesService, val invoice: Invoice, val targetCurrency: String, language: String
+	) {
+	val targetCurrencyUpperCase = targetCurrency.toUpperCase()
+	private val locale = new Locale(language, language)
+	private val quantityFormatter = NumberFormat.getIntegerInstance(locale)
+	quantityFormatter.setMaximumFractionDigits(2)
+	private val pricesFormatter = NumberFormat.getIntegerInstance(locale)
+	pricesFormatter.setMinimumFractionDigits(2)
+	pricesFormatter.setMaximumFractionDigits(2)
+	private val curencyRateFormatter = NumberFormat.getIntegerInstance(locale)
+	curencyRateFormatter.setMinimumFractionDigits(4)
+	curencyRateFormatter.setMaximumFractionDigits(4)
+	
+	val paymentDescription: String = externalTemplatesService.get(
+		"payments.".concat(invoice.paymentMethod.symbol).concat("-description-html-").concat(language.toLowerCase())
+	).map { template =>
+		template
+	}.getOrElse("")
+	
+	val paymentName: String = externalTemplatesService.get(
+		"payments.".concat(invoice.paymentMethod.symbol).concat("-name-").concat(language.toLowerCase())
+	).map { template =>
+		template
+	}.getOrElse(invoice.paymentMethod.name)
+	
+	def quantity(invoicePart: InvoicePart): String = {
+		quantityFormatter.format(BigDecimal(invoicePart.quantity) / InvoicePart.Multipler)
+	}
+	
+	def unitPrice(invoicePart: InvoicePart): String = {
+		pricesFormatter.format(BigDecimal(invoicePart.unitPrice) / InvoicePart.Multipler).concat(" ").concat(invoice.currency)
+	}
+	
+	def total(invoicePart: InvoicePart): String = {
+		pricesFormatter.format(BigDecimal(invoicePart.total) / InvoicePart.Multipler).concat(" ").concat(invoice.currency)
+	}
+	
+	val invoiceTotal: String = {
+		pricesFormatter.format(BigDecimal(invoice.total) / Invoice.Multipler).concat(" ").concat(invoice.currency)
+	}
+	
+	val currencyRate: Option[CurrencyRate] = currenciesService.getFromPreviousDay(invoice.issueDate, targetCurrency, invoice.currency)
+	
+	val currencyRateValue: Option[BigDecimal] = {
+		if (targetCurrencyUpperCase == invoice.currency.toUpperCase())
+			Some(BigDecimal(1))
+		else
+			currencyRate.map { currencyRate => 
+				Some(BigDecimal(currencyRate.rate) / CurrencyRate.MultiplerValue)
+			}.getOrElse(None)
+	}
+	
+	val currencyRateFormatted: String = {
+		currencyRate.map { currencyRate =>
+			curencyRateFormatter.format(BigDecimal(currencyRate.rate) / CurrencyRate.MultiplerValue)
+		}.getOrElse("")
+	}
+	
+	val invoiceTotalConvertedValue: Option[BigDecimal] = {
+		currencyRateValue.map { rateValue =>
+			Some(BigDecimal(invoice.total) / Invoice.Multipler * rateValue)
+		}.getOrElse(None)
+	}
+	
+	val invoiceTotalConverted: Option[String] = {
+		invoiceTotalConvertedValue.map { amount =>
+			Some(pricesFormatter.format(amount).concat(" ").concat(targetCurrencyUpperCase))
+		}.getOrElse(None)
+	}
+	
+}
